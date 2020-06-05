f15e6e9b7fda2ac37fe18224fc3a203530d0e200
Sini <saasini@ad.helsinki.fi>
2020-06-05 12:42:06 +0300

    Dijkstran algoritmin testailua

--- Reitinhaku/src/main/java/tiralabra/reitinhaku/main/Main.java
+++ Reitinhaku/src/main/java/tiralabra/reitinhaku/main/Main.java
@@ -5,10 +5,45 @@
  */
 package tiralabra.reitinhaku.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tiralabra.reitinhaku.algoritmit.Dijkstra;
+import tiralabra.reitinhaku.kartat.Kartanlukija;
+import tiralabra.reitinhaku.tietorakenteet.Solmu;
+
 /**
  *
  * @author saasini
  */
 public class Main {
-    
+
+    public static void main(String[] args) throws FileNotFoundException, Exception {
+        File kartta = new File("./kartat/London_2_256.map");
+        Kartanlukija kartanlukija = new Kartanlukija(kartta);
+        char[][] valittuKartta = kartanlukija.muutaMatriisiksi();
+        
+        Solmu alku = new Solmu(5, 2);
+        Solmu loppu = new Solmu(10, 2);
+        Dijkstra dijkstra = new Dijkstra(valittuKartta);
+        
+        long aloitusAika = System.currentTimeMillis();
+        int lyhinReitti = dijkstra.laskeReitinPituus(alku, loppu);
+        long lopetusAika = System.currentTimeMillis();
+        
+        long aloitusAika2 = System.currentTimeMillis();
+        //A*-haku
+        long lopetusAika2 = System.currentTimeMillis();
+
+
+        System.out.println();
+        System.out.println("Lyhin reitti Dijkstran algoritmilla oli pituudeltaan " + lyhinReitti);
+        //System.out.println("Lyhin reitti A*-algoritmin etsimänä oli pituudeltaan  " + lyhinReitti2);
+
+        System.out.println();
+        System.out.println("Suoritusaika Dijkstran algoritmille oli " + (lopetusAika - aloitusAika) + "ms.");
+        //System.out.println("Suoritusaika A*-algoritmille oli " + (lopetusAika2 - aloitusAika2) + "ms.");
+    } 
 }
