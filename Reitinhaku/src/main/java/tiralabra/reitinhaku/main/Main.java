a738016bcca48b91f31b3526a21c6449a13d7040
Sini <saasini@ad.helsinki.fi>
2020-06-27 13:40:53 +0300

    lisää tiedostoja

--- Reitinhaku/src/main/java/tiralabra/reitinhaku/main/Main.java
+++ Reitinhaku/src/main/java/tiralabra/reitinhaku/main/Main.java
@@ -5,14 +5,21 @@
  */
 package tiralabra.reitinhaku.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import tiralabra.reitinhaku.algoritmit.AStar;
+import tiralabra.reitinhaku.algoritmit.Dijkstra;
+import tiralabra.reitinhaku.kartat.Kartanlukija;
+import tiralabra.reitinhaku.tietorakenteet.Solmu;
 import tiralabra.reitinhaku.ui.Kayttoliittyma;
+
 /**
  *
  * Main-luokka käynnistää käyttöliittymän.
  */
 public class Main {
-
-    public static void main(String[] args) {
+    
+    public static void main(String[] args) throws FileNotFoundException, Exception {
         Kayttoliittyma.main(args);
     }
 }
